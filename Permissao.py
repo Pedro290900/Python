39788aea62e6be6d5e5c09340c0bd82db0a45aff
Pedro290900 <pcoutinhoduarte@gmail.com>
2024-10-04 22:06:43 -0300

    Create Permissao.py

---
+++ Permissao.py
@@ -0,0 +1,8 @@
+int idade
+idade = int(input("Digite sua idade"))
+if idade < 18:
+  print("Permissão negada")
+  print("Idade mínima 18")
+else:
+  print("Acesso permitido")
+  print("Seja Bem vindo")
